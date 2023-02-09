d6e5727f94b11970d8f74a04f9bf596265d38b83
Fadi Hassoun <fadi.hassoun.sd@gmail.com>
2023-02-09 12:29:29 +0200

    first commit

---
+++ exercise_files/01_04/hello.py
@@ -0,0 +1,20 @@
+
+# Print "Hello, World!" to the terminal
+#print('Hello, World!')
+# lines = []
+# notblank = True
+# while notblank:
+#     line = input()
+#     if line != "":
+#         lines.append(line)
+#     else:
+#         notblank =False
+# for x in lines:
+#     print(x)
+
+no_of_lines = 2
+lines = []
+for i in range(no_of_lines):
+    lines.append(input())
+print(lines[0])
+print(lines)
